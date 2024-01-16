2c1a2f05873675f2af2271572e83f42fd331008c
laksanautama <kussa_laksana@uhnsugriwa.ac.id>
2024-01-17 00:48:37 +0800

    submit file cpp and pdf

---
+++ kembalian_belanja.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+
+using namespace std;
+
+//----Tidak boleh menambah kode di atas batas ini-----
+
+void status_kembalian(){
+
+//CODE
+}
+
+
+int main(){
+
+return 0;
+}
