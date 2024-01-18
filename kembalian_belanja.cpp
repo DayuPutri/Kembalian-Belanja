d49dc3bd319c92cd8e8029576cb91b7bac4c678d
payanti <dwipayanti288@gmail.com>
2024-01-18 12:13:41 +0800

    kembalian_belanja baru
    Ari Dwipayanti

--- kembalian_belanja.cpp
+++ kembalian_belanja.cpp
@@ -7,13 +7,35 @@ using namespace std;
 
 //----Tidak boleh menambah kode di atas batas ini-----
 
-void status_kembalian(){
+void status_kembalian(int a, int n, int uang[]){
+    //Menggunakan strategi Greedy untuk memberi kembalian
+    map<int, int> count; //Menggunakan map untuk menghitung jumlah setiap pecahan uang
 
-//CODE
+    for (int i = 0; i < n; i++) {
+            count[uang]]++;
+    }
+
+    int kembalian = 0;
+    for (auto it = count.rbegin(); it != count.rend(); ++it) {
+        int nominal = it->first;
+        int jumlah = it->secound;
+
+        while (kembalian + nominal <= a && jumlah > 0) {
+            kembalian += nominal;
+            jumlah--;
+        }
+    }
+
+    if (kembalian == a) {
+        cout << "Kembalian sesuai." << endl;
+    } else {
+        cout << "Kemampuan tidak sesuai." << endl;
+    }
 }
 
 
 int main(){
+    int a = 5000;
 
 return 0;
 }
