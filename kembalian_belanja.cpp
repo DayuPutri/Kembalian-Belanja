5809cb5853862b966533eec3d8cfd536a3c14247
DayuPutri <dayu81096@gmail.com>
2024-01-18 12:47:38 +0800

    Kembalian_belanja
    dayu putri

--- kembalian_belanja.cpp
+++ kembalian_belanja.cpp
@@ -5,18 +5,39 @@
 
 using namespace std;
 
-void status_kembalian(int a, int n, int uang[]){
-    //Menggunakan strategi Greedy untuk memberi kembalian
-    map<int, int> count;  //Menggunakan map untuk menghitung jumlah setiap pecahan uang
-    for (int i = 0; i < n; i++){
+void status_kembalian(int a, int n, int uang[]) {
+    // Menggunakan strategi Greedy untuk memberi kembalian
+    map<int, int> count;  // Menggunakan map untuk menghitung jumlah setiap pecahan uang
+
+    for (int i = 0; i < n; i++) {
         count[uang[i]]++;
     }
 
-//CODE
+    int kembalian = 0;
+    for (auto it = count.rbegin(); it != count.rend(); ++it) {
+        int nominal = it->first;
+        int jumlah = it->second;
+
+        while (kembalian + nominal <= a && jumlah > 0) {
+            kembalian += nominal;
+            jumlah--;
+        }
+    }
+
+    if (kembalian == a) {
+        cout << "Kembalian sesuai." << endl;
+    } else {
+        cout << "Kembalian tidak sesuai." << endl;
+    }
 }
 
+int main() {
+    int a = 5000;
+    int n = 5;
+    int uang[] = {5000, 5000, 5000, 10000, 20000};
 
-int main(){
+    // Memanggil fungsi status_kembalian
+    status_kembalian(a, n, uang);
 
 return 0;
 }
