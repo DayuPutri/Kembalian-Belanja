7912e4269fb830d50bd075b20c881ca2cfa2c1e5
DayuPutri <dayu81096@gmail.com>
2024-01-18 10:08:33 +0800

    Update kembalian_belanja.cpp
    Om swastyastu bapak saya mengerjakan dari void status_kembalian ini suksema
    
    Co-Authored-By: laksanautama <155938070+laksanautama@users.noreply.github.com>

--- kembalian_belanja.cpp
+++ kembalian_belanja.cpp
@@ -5,9 +5,12 @@
 
 using namespace std;
 
-//----Tidak boleh menambah kode di atas batas ini-----
-
-void status_kembalian(){
+void status_kembalian(int a, int n, int uang[]){
+    //Menggunakan strategi Greedy untuk memberi kembalian
+    map<int, int> count;  //Menggunakan map untuk menghitung jumlah setiap pecahan uang
+    for (int i = 0; i < n; i++){
+        count[uang[i]]++;
+    }
 
 //CODE
 }
